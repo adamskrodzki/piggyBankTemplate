4126a4cd80d873cb920b0f4117ecf3129818f982
Adam Skrodzki <adamskrodzki@gmail.com>
2019-01-25 07:10:30 +0000

    small fix

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import { buildServices } from "./app/configuration/serviceBuilder";
 import { setupComponents } from "./app/configuration/componentBuilder";
 import { setupRoutes } from "./app/configuration/routesConfig";
 
+document.addEventListener("DOMContentLoaded", function(event) {
 setupApp();
+});
 
 function setupApp() {
   const app = new App("#app");
